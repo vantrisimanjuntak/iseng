a48a7f0cdb6c8d43a8fefed880b827da9cadfe61
Vantri Simanjuntak <vantri_antonius@yahoo.co.id>
2022-10-17 23:37:17 +0700

    Setting page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WPU Blog | About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3>Yangseku</h3>
+    <p>Yang selalu kusayang</p>
+</body>
+</html>
